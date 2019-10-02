22e1afe87793e84cb4deb9ce53c64da7f470e577
noor bondogji <noor.bondogji@sjsu.edu>
2019-10-01 22:32:01 -0700

    fixing it

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(193, 176, 255);
+
+  translate(-width/2, -height/2);
+    noStroke();
+
+  strokeWeight(5)
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed == true){
+    stroke(map(mouseX, 0, 600, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - mouseX, height- pmouseY);
+    //line(mouseX, mouseY, mouseX, pmouseY);
+
+  }
+
 }
