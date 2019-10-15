0c2b1cc70c7b0ea99364f55243d7ed739abe3501
noor bondogji <noor.bondogji@sjsu.edu>
2019-10-15 00:04:44 -0700

    Update sketchg.js

--- sketchg.js
+++ sketchg.js
@@ -19,12 +19,6 @@ fill(value);
 
 function draw() {
 
-  for (let x=0; x < width; x++) {
-    let noiseVal = noise((mouseX+x)*noiseScale, mouseY*noiseScale);
-    stroke(noiseVal*255);
-    line(x, mouseY+noiseVal*80, x, height);
-  }
-}
 
    //line(mouseX, 0, mouseX, 360);
 
